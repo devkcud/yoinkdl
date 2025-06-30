1a89b396f0adff7038018dfba6812977de4de4fb
André devkcud Albanese Junior <patommmmm@proton.me>
2025-06-29 22:03:12 -0300

    feat(filename): Add `ToMime()` method

--- pkg/filename/model.go
+++ pkg/filename/model.go
@@ -1,20 +1,25 @@
 package filename
 
-import "fmt"
+import (
+	"fmt"
+	"mime"
+)
 
 type Basic struct {
 	Name      string
 	Extension string
 }
 
+var Default = MustNew("download_file")
+
 // If entryCopy is defined (aka >0), it appends '_{entryCopy}' between Name and Extension
-func (f *Basic) Full(entryCopy ...int) string {
+func (b *Basic) Full(entryCopy ...int) string {
 	if len(entryCopy) != 0 {
-		return fmt.Sprintf("%s_%d%s", f.Name, entryCopy[0], f.Extension)
+		return fmt.Sprintf("%s_%d%s", b.Name, entryCopy[0], b.Extension)
 	}
-	return fmt.Sprintf("%s%s", f.Name, f.Extension)
+	return fmt.Sprintf("%s%s", b.Name, b.Extension)
 }
 
-func NewDefault() *Basic {
-	return &Basic{Name: "download_file", Extension: ".goondl"}
+func (b *Basic) ToMime() string {
+	return mime.TypeByExtension(b.Extension)
 }
